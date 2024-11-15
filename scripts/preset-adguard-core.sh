8ec5c9412bbc4d4620a50c571c37883b7541b18f
haiibo <boyhaibo@gmail.com>
2024-11-15 12:55:49 +0800

    Initial commit

--- scripts/preset-adguard-core.sh
+++ scripts/preset-adguard-core.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mkdir -p files/usr/bin/AdGuardHome
-AGH_CORE=$(curl -sL https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest | grep /AdGuardHome_linux_${1} | awk -F '"' '{print $4}')
+AGH_CORE="https://github.com/AdguardTeam/AdGuardHome/releases/latest/download/AdGuardHome_linux_${1}.tar.gz"
 wget -qO- $AGH_CORE | tar xOvz > files/usr/bin/AdGuardHome/AdGuardHome
 chmod +x files/usr/bin/AdGuardHome/AdGuardHome
